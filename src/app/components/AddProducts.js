4bbbdb95a7c2c3748ca55c5c35a872922778b92a
developerrakesh93 <78033683+developerrakesh93@users.noreply.github.com>
2023-10-01 23:43:27 +0530

    add data to store

--- src/app/components/AddProducts.js
+++ src/app/components/AddProducts.js
@@ -1,6 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../redux/slice";
 
 const AddProducts = () => {
+  const dispatcher = useDispatch();
+  const [productName, setProductName] = useState("");
+  const userDispatch = () => {
+    dispatcher(addProduct(productName));
+  };
+
   return (
     <div className="add-product">
       <h3>Add New Product</h3>
@@ -8,8 +17,11 @@ const AddProducts = () => {
         type="text"
         placeholder="Add New product"
         className="add-product-input"
+        onChange={(e) => setProductName(e.target.value)}
       />
-      <button className="add-product-btn">Add Product</button>
+      <button onClick={userDispatch} className="add-product-btn">
+        Add Product
+      </button>
     </div>
   );
 };
