23da8fb0b8a71cf25af2fe22caa84893e504d928
developerrakesh93 <78033683+developerrakesh93@users.noreply.github.com>
2023-10-02 19:56:53 +0530

    changes

---
+++ src/app/components/AddProducts.js
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../redux/slice";
+
+const AddProducts = () => {
+  const dispatcher = useDispatch();
+  const [productName, setProductName] = useState("");
+  const userDispatch = () => {
+    dispatcher(addProduct(productName));
+  };
+
+  return (
+    <div className="add-product">
+      <h3>Add New Product</h3>
+      <input
+        type="text"
+        placeholder="Add New product"
+        className="add-product-input"
+        onChange={(e) => setProductName(e.target.value)}
+      />
+      <button onClick={userDispatch} className="add-product-btn">
+        Add Product
+      </button>
+    </div>
+  );
+};
+
+export default AddProducts;
